83d02e6830145bc5c12ec8f559552e441742d126
Yury Barysevich <kassik0boks@gmail.com>
2025-06-01 23:52:44 +0300

    feat: added ability to use env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 4000;
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(port);
+  console.log(`App started on ${port} port`);
 }
 bootstrap();
