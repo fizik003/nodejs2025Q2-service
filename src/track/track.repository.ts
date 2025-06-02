222d2576aa352f7acc5e799afc72f10ffe505479
Yury Barysevich <kassik0boks@gmail.com>
2025-06-02 22:50:05 +0300

    feat: add album resource

--- src/track/track.repository.ts
+++ src/track/track.repository.ts
@@ -4,15 +4,15 @@ export class TrackRepository {
   private tracks: Track[] = [
     {
       id: '701e80f9-7b04-4c64-a720-6bbd22533daf',
-      albumId: null,
-      artistId: null,
+      albumId: '672fabab-e693-4b39-9240-1631e0d496d6',
+      artistId: 'c537a02b-4fbc-4cfa-94cb-32d18635a0a5',
       duration: 100,
       name: 'Track1',
     },
     {
       id: '3108e644-8be0-404d-8194-5f5f9e66d694',
-      albumId: null,
-      artistId: null,
+      albumId: '68255a8e-038a-40d9-bec8-83c9d4620d0c',
+      artistId: '7f7be693-ccee-44be-b0f1-e7884e616239',
       duration: 200,
       name: 'Track2',
     },
